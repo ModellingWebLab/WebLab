3c976f685dea07a831954d313bb9762faf85311b
Jonathan Cooper <j.p.cooper@ucl.ac.uk>
2018-02-05 16:21:29 +0000

    Define STATIC_ROOT for collectstatic

--- weblab/config/settings/base.py
+++ weblab/config/settings/base.py
@@ -160,6 +160,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = str(BASE_DIR / 'staticfiles')
 STATICFILES_DIRS = [
     str(BASE_DIR / 'static'),
 ]
