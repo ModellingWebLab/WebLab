e31d32931bc542ef5fa2b8999fcf198fce303baa
Jonathan Cooper <j.p.cooper@ucl.ac.uk>
2018-02-05 13:39:17 +0000

    Restructure config to help with deployment
    Ansible will generate deployed.py and secrets.py based on configured
    variables.

--- weblab/config/settings/dev.py
+++ weblab/config/settings/dev.py
@@ -3,9 +3,6 @@ from .base import *  # noqa
 
 DEBUG = True
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dummy-secret-key'
-
 ALLOWED_HOSTS = ['*']
 
 # Log all emails to console
