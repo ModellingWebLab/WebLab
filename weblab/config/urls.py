6457f58720acf08d5d183d7ddb81814b9aef1028
Jonathan Cooper <j.p.cooper@ucl.ac.uk>
2018-05-15 09:20:39 +0100

    Add an about page
    The content is mostly the same as the WL1 version, with slight tweaks
    to reflect the fact that this is the WL2 codebase.

--- weblab/config/urls.py
+++ weblab/config/urls.py
@@ -20,6 +20,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='home.html'), name="home"),
+    url(r'^about$', TemplateView.as_view(template_name='about.html'), name="about"),
     url(r'^accounts/', include('accounts.urls', namespace='accounts')),
     url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^social/', include('social_django.urls', namespace='social')),
