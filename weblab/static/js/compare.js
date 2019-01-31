762d6cef143cdf6352e69cd5b621e7c401a00aed
Helen Sherwood-Taylor <helen@rrdlabs.co.uk>
2019-01-31 08:52:36 +0000

    Allow protocol diffs too

--- weblab/static/js/compare.js
+++ weblab/static/js/compare.js
@@ -602,7 +602,7 @@ function parseUrl (event)
       basicurl = parts.slice(0, i+2).join('/') + '/';
       entityType = 'experiment';
       entityIds = parts.slice(i+2);
-    } else if (parts[i] == 'models' && parts[i+1] == 'compare') {
+    } else if ((parts[i] == 'models' || parts[i] == 'protocols') && parts[i+1] == 'compare') {
       basicurl = parts.slice(0, i+2).join('/') + '/';
       entityType = parts[i].slice(0, parts[i].length-1);
       entityIds = parts.slice(i+2);
