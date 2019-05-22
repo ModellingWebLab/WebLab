1df69bc2182ad21905e213653bf03713c2f2649d
Jonathan Cooper <j.p.cooper@ucl.ac.uk>
2019-05-22 09:18:35 +0100

    Log debug info by default on local Django

--- weblab/config/settings/dev.py
+++ weblab/config/settings/dev.py
@@ -1,7 +1,11 @@
+import copy
+import sys
+
 from .base import *  # noqa
 
 
 DEBUG = True
+LOG_DEBUG = DEBUG
 
 ALLOWED_HOSTS = ['*']
 
@@ -18,3 +22,53 @@ ROLLBAR = {
     'branch': 'master',
     'root': str(BASE_DIR),  # noqa
 }
+
+# Logging settings
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'stream': sys.stdout,
+            'formatter': 'verbose',
+        },
+        'console_err': {
+            'level': 'WARNING',
+            'class': 'logging.StreamHandler',
+            'stream': sys.stderr,
+            'formatter': 'verbose',
+        },
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(name)s: %(message)s',
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s',
+        },
+    },
+    'loggers': {
+        # Root logger
+        '': {
+            'handlers': ['console'],
+            'disabled': False,
+            'level': 'DEBUG' if LOG_DEBUG else 'INFO',
+        },
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG' if LOG_DEBUG else 'INFO',
+            'propagate': False,
+        },
+    }
+}
+
+local_logger_conf = {
+    'handlers': ['console'],
+    'level': 'DEBUG' if LOG_DEBUG else 'INFO',
+}
+LOGGING['loggers'].update({
+    app.split('.')[0]: copy.deepcopy(local_logger_conf)
+    for app in LOCAL_APPS
+})
