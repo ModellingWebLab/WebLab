427e3342fa1d90c581f6fbfa82754d16508bb70f
Sarah <s.keating@ucl.ac.uk>
2019-06-11 13:45:38 +0100

    added tests for dataset model

--- weblab/datasets/tests/test_models.py
+++ weblab/datasets/tests/test_models.py
@@ -14,3 +14,29 @@ class TestDataset:
         dataset = recipes.dataset.make(name='test dataset')
         assert str(dataset) == 'test dataset'
 
+    def test_is_visible_to_user(self, user, other_user):
+        dataset = recipes.dataset.make(author=user, name='mydataset', visibility="private")
+        assert dataset.is_visible_to_user(user)
+
+        assert not dataset.is_visible_to_user(other_user)
+
+    def test_related_protocl(self, user):
+        protocol = recipes.protocol.make(author=user)
+        dataset = recipes.dataset.make(author=user, name='mydataset', protocol=protocol)
+
+        assert dataset.protocol == protocol
+
+
+
+@pytest.mark.django_db
+class TestDatasetNameUniqueness:
+    def test_user_cannot_have_same_named_dataset(self, user):
+        recipes.dataset.make(author=user, name='mydataset')
+
+        with pytest.raises(IntegrityError):
+            ExperimentalDataset.objects.create(author=user, name='mydataset')
+
+    def test_different_users_can_have_same_named_model(self, user, other_user):
+        recipes.dataset.make(author=user, name='mydataset')
+        other_dataset = recipes.dataset.make(author=other_user, name='mydataset')
+        assert other_dataset
