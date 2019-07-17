71897bce32eb1918376125201e5d939e31f2d77a
Helen Sherwood-Taylor <helen@rrdlabs.co.uk>
2019-07-17 11:29:04 +0000

    Support multiple javascript bundles in gulpfile

--- weblab/static/gulpfile.js
+++ weblab/static/gulpfile.js
@@ -4,13 +4,14 @@ var watchify = require('watchify');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
-var log = require('fancy-log');
 var sourcemaps = require('gulp-sourcemaps');
-var assign = require('lodash.assign');
+var es = require('event-stream');
 
 var config = {
   js: {
-    src: './js/main.js',
+    src: [
+      './js/main.js',
+    ],
     outputDir: './build/',
     mapDir: './maps/',
   },
@@ -21,44 +22,59 @@ var config = {
   }
 }
 
-gulp.task('sass', function () {
-  return gulp.src(config.sass.src)
+
+gulp.task('sass', () =>
+  gulp.src(config.sass.src)
     .pipe(sass({includePaths: config.sass.include}))
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest(config.sass.dest));
-});
+    .pipe(gulp.dest(config.sass.dest))
+);
+
+gulp.task('watch:sass', () =>
+  gulp.watch(config.sass.src, gulp.series('sass'))
+);
 
 
-var browserified = function() {
-  return browserify({
-    cache: {},
-    packageCache: {},
-    entries: [config.js.src],
-    debug: true,
-    transform: []
-  });
+var buildJs = (watch, done) => {
+    // map each js source file to a stream
+    var tasks = config.js.src.map((entry) => {
+      var bundler = browserify({
+        entries: [entry],
+        cache: {},
+        packageCache: {},
+        debug: true,
+        transform: []
+      });
+
+      var bundle = () => bundler.bundle()
+          .pipe(source(entry))
+          .pipe(buffer())
+          .pipe(sourcemaps.init({ loadMaps : true }))
+          .pipe(sourcemaps.write(config.js.mapDir))
+          .pipe(gulp.dest(config.js.outputDir));
+
+      if (watch) {
+        bundler = watchify(bundler);
+        bundler.on('update', bundle);
+        bundler.on('log', console.log);
+      }
+
+      return bundle();
+    });
+
+    // create a merged stream
+    merged = es.merge(tasks);
+
+    if (watch) {
+      return merged;
+    } else {
+      return merged.on('end', done);
+    }
 };
-var watchified = watchify(browserified());
 
-var bundle = function(pkg) {
-  return pkg.bundle()
-      .pipe(source(config.js.src))
-      .pipe(buffer())
-      .pipe(sourcemaps.init({ loadMaps : true }))
-      .pipe(sourcemaps.write(config.js.mapDir))
-      .pipe(gulp.dest(config.js.outputDir));
-};
-gulp.task('js', bundle.bind(null, browserified()));
- 
-
-gulp.task('watch', function () {
-  gulp.watch(config.sass.src, gulp.series('sass'));
-  // subscribe to JS file updates.
-  // bundle() must be called in order to start emitting update events
-  bundle(watchified);
-  watchified.on('update', bundle.bind(null, watchified));
-  watchified.on('log', log);
-});
+gulp.task('js', (done) => buildJs(false, done));
+gulp.task('watch:js', (done) => buildJs(true, done));
 
 
-gulp.task('default', gulp.series(gulp.parallel('sass', 'js')));
+gulp.task('watch', gulp.parallel('watch:js', 'watch:sass'));
+gulp.task('default', gulp.parallel('sass', 'js'));
