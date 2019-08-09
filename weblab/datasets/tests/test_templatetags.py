2b889e7717f87ed219963864233b54294ec49136
Sarah <s.keating@ucl.ac.uk>
2019-08-09 11:14:38 +0100

    finally sussed how to create a dummy file upload to a dataset for testing

--- weblab/datasets/tests/test_templatetags.py
+++ weblab/datasets/tests/test_templatetags.py
@@ -1,7 +1,6 @@
 import pytest
 
 import datasets.templatetags.datasets as dataset_tags
-from datasets.models import Dataset
 
 from core import recipes
 
@@ -12,12 +11,22 @@ def test_dataset_urls_no_files(dataset_no_files):
     assert dataset_tags.url_dataset(dataset) == '/datasets/%d/addfiles' % dataset.pk
 
 
-# @pytest.mark.django_db
-# def test_dataset_urls_with_files(dataset_dummy_files):
-#     dataset = dataset_dummy_files
-#     assert dataset_tags.url_dataset(dataset) == '/datasets/%d' % dataset.pk
-#
-#
+@pytest.mark.django_db
+def test_dataset_urls_with_files(logged_in_user, public_protocol, client):
+    dataset = recipes.dataset.make(author=logged_in_user, protocol=public_protocol)
+    # do a dummy upload of a file
+    client.post(
+        '/datasets/%d/addfiles' % dataset.pk,
+        data={
+            'filename[]': ['uploads/file1.csv'],
+            'delete_filename[]': [],
+            'mainEntry': ['file1.csv'],
+        },
+    )
+
+    assert dataset_tags.url_dataset(dataset) == '/datasets/%d' % dataset.pk
+
+
 @pytest.mark.django_db
 def test_can_create_dataset_no_permission(user, helpers):
     context = {'user': user}
