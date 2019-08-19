3db33d465d4f93fc5a25642ac22679ab4e5f356a
Sarah <s.keating@ucl.ac.uk>
2019-08-19 12:35:06 +0100

    check whether my issue is a windows related issue

--- weblab/datasets/tests/test_views.py
+++ weblab/datasets/tests/test_views.py
@@ -141,40 +141,62 @@ class TestDatasetFileDownloadView:
         )
         assert response['Content-Type'] == 'text/csv'
 
-#     @pytest.mark.parametrize("filename", [
-#         ('oxmeta:membrane-voltage with spaces.csv'),
-#         ('oxmeta%3Amembrane_voltage.csv'),
-#     ])
-#     def test_handles_odd_characters(self, client, helpers, filename):
-#         model = recipes.model.make()
-#         v1 = helpers.add_version(model, visibility='public', filename=filename)
-#
-#         response = client.get(
-#             reverse('entities:file_download', args=['model', model.pk, v1.hexsha, filename])
-#         )
-#
-#         assert response.status_code == 200
-#         assert response.content == b'entity contents'
-#         assert response['Content-Disposition'] == (
-#             'attachment; filename=' + filename
-#         )
-#         assert response['Content-Type'] == 'text/csv'
-#
-#     @pytest.mark.parametrize("filename", [
 
-#         ('/etc/passwd'),
-#         ('../../../../../pytest.ini'),
-#     ])
-#     def test_disallows_non_local_files(self, client, public_model, filename):
-#         version = public_model.repo.latest_commit
-#
-#         response = client.get(
-#             '/entities/models/%d/versions/%s/download/%s' %
-#             (public_model.pk, version.hexsha, filename)
-#         )
-#
-#         assert response.status_code == 404
-#
+    @pytest.mark.parametrize("filename", [
+        ('oxmeta:membrane-voltage with spaces.csv'),
+        ('oxmeta%3Amembrane_voltage.csv'),
+    ])
+    def test_handles_odd_characters(self, logged_in_user, helpers, client, public_protocol, filename):
+        dataset_name = filename[0:len(filename)-4]
+        helpers.add_permission(logged_in_user, 'create_dataset', Dataset)
+        dataset = recipes.dataset.make(author=logged_in_user, name=dataset_name, protocol=public_protocol)
+        file_contents = b'my test dataset'
+        recipes.dataset_file.make(
+            dataset=dataset,
+            upload=SimpleUploadedFile(filename, file_contents),
+            original_name=filename,
+        )
+        client.post(
+            '/datasets/%d/addfiles' % dataset.pk,
+            data={
+                'filename[]': ['uploads/' + filename],
+                'delete_filename[]': [],
+                'mainEntry': [filename],
+            },
+        )
+
+        # neither of these get methods work
+
+
+        # response = client.get(
+        #     '/datasets/%d/download/%s' % (dataset.pk, filename)
+        # )
+
+        # this gives exception No reverse match
+        response = client.get(
+            reverse('datasets:file_download', args=['dataset', dataset.pk, filename])
+        )
+
+        assert response.status_code == 200
+#        assert response.status_code == 404
+        # assert response.content == b'my test dataset'
+        # assert response['Content-Disposition'] == (
+        #     'attachment; filename=' + filename
+        # )
+        # assert response['Content-Type'] == 'text/csv'
+
+    # @pytest.mark.parametrize("filename", [
+    #     ('/etc/passwd'),
+    #     ('../../../../../pytest.ini'),
+    # ])
+    # def test_disallows_non_local_files(self, client, my_dataset_with_file, filename):
+    #     response = client.get(
+    #         '/datasets/%d/download/%s' %
+    #         (my_dataset_with_file.pk, filename)
+    #     )
+    #
+    #     assert response.status_code == 404
+    #
 #     @patch('mimetypes.guess_type', return_value=(None, None))
 #     def test_uses_octet_stream_for_unknown_file_type(self, mock_guess, client, public_model):
 #         version = public_model.repo.latest_commit
