33c7b47e119955d9a85160042efe47ccb05b3097
Jonathan Cooper <j.p.cooper@ucl.ac.uk>
2019-12-04 13:49:11 +0000

    Add protocol field to create fitting spec form
    It uses the new helpers to filter out invisible protocols

--- weblab/fitting/forms.py
+++ weblab/fitting/forms.py
@@ -1,4 +1,5 @@
 from entities.forms import EntityForm
+from entities.models import ProtocolEntity
 
 from .models import FittingSpec
 
@@ -7,7 +8,11 @@ class FittingSpecForm(EntityForm):
     """Used for creating an entirely new fitting specification."""
     class Meta:
         model = FittingSpec
-        fields = ['name']
+        fields = ['name', 'protocol']
 
-    # TODO: Add protocol link, like for datasets (ensuring visibility respected)
-    # Perhaps sort available protocols so 'mine' first, then moderated, then others?
+    def __init__(self, *args, **kwargs):
+        """Only show visible protocols in the selection."""
+        super().__init__(*args, **kwargs)
+        self.fields['protocol'].queryset = ProtocolEntity.objects.visible_to_user(self.user)
+
+    # TODO: Perhaps sort available protocols so 'mine' first, then moderated, then others?
