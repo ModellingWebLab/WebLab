8f86c588e6062523eb058b0b256087d86a743811
steve@roderick.com <steve@roderick.com>
2020-01-27 14:53:37 +0000

    Reverted changes to test.py

--- weblab/config/settings/test.py
+++ weblab/config/settings/test.py
@@ -1,37 +1,4 @@
-from . import base
+from .base import *  # noqa
+
 
 # Settings file used by pytest, whether locally or on Travis
-
-# Test settings
-SECRET_KEY = 'test-secret-key'
-
-
-class InvalidStringShowWarning(str):
-    def __mod__(self, other):
-        import logging
-        logger = logging.getLogger(__name__)
-        logger.warning("In template, undefined variable or unknown value for: '%s'" % (other,))
-        return ""
-
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            base.BASE_DIR / 'templates',
-        ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'string_if_invalid': InvalidStringShowWarning("%s"),
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'social_django.context_processors.backends',
-                'social_django.context_processors.login_redirect',
-                'core.context_processors.common',
-            ],
-        },
-    },
-]
