20753cf6cb2584fafa45120ddab470fa3f8916a4
steve@roderick.com <steve@roderick.com>
2020-01-27 11:42:45 +0000

    Added template option to alert for invalid strings

--- weblab/config/settings/dev.py
+++ weblab/config/settings/dev.py
@@ -80,3 +80,34 @@ LOGGING['loggers'].update({
     app.split('.')[0]: copy.deepcopy(local_logger_conf)
     for app in LOCAL_APPS
 })
+
+
+class InvalidStringShowWarning(str):
+    def __mod__(self, other):
+        import logging
+        logger = logging.getLogger(__name__)
+        logger.warning("In template, undefined variable or unknown value for: '%s'" % (other,))
+        return ""
+
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'string_if_invalid': InvalidStringShowWarning("%s"),
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
+                'core.context_processors.common',
+            ],
+        },
+    },
+]
--- weblab/config/settings/test.py
+++ weblab/config/settings/test.py
@@ -2,3 +2,32 @@ from .base import *  # noqa
 
 
 # Settings file used by pytest, whether locally or on Travis
+class InvalidStringShowWarning(str):
+    def __mod__(self, other):
+        import logging
+        logger = logging.getLogger(__name__)
+        logger.warning("In template, undefined variable or unknown value for: '%s'" % (other,))
+        return ""
+
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'string_if_invalid': InvalidStringShowWarning("%s"),
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
+                'core.context_processors.common',
+            ],
+        },
+    },
+]
