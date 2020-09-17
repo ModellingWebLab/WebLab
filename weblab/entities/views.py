71778aeeb34bd64f1b144c5ed998fdd646923208
Steve Roderick <steve@roderick.com>
2020-09-17 17:31:40 +0100

    Whitespace issue

--- weblab/entities/views.py
+++ weblab/entities/views.py
@@ -777,6 +777,7 @@ class EntityFileDownloadView(EntityTypeMixin, EntityVersionMixin, SingleObjectMi
 
         return response
 
+
 class RenameView(LoginRequiredMixin, UserPassesTestMixin, FormMixin, EntityTypeMixin, DetailView):
     template_name = 'entities/entity_rename_form.html'
     context_object_name = 'entity'
