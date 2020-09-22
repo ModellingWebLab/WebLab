2701ff86942133e6232e091231af43c047603747
Steve Roderick <steve@roderick.com>
2020-09-22 10:38:36 +0100

    Changed tag validation message

--- weblab/entities/forms.py
+++ weblab/entities/forms.py
@@ -95,7 +95,9 @@ class EntityTagVersionForm(forms.Form):
     tag = forms.CharField(
         label='New tag',
         help_text='Short label for this version',
-        required=True, validators=[RegexValidator(r'[-_A-Za-z0-9]', 'Please enter a valid tag name.')])
+        required=True,
+        validators=[RegexValidator(r'[-_A-Za-z0-9]', 'Please enter a valid tag name.'
+                                                     ' Only letters, numbers, dashes or underscores are allowed.')])
 
 
 class EntityCollaboratorForm(forms.Form):
