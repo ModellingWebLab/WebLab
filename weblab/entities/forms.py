b6bd8f3e48976625a4b5a6c24bfe90268530d733
Steve Roderick <steve@roderick.com>
2020-10-01 15:25:23 +0100

    Reverted accidental checkin

--- weblab/entities/forms.py
+++ weblab/entities/forms.py
@@ -14,7 +14,7 @@ class EntityForm(UserKwargModelFormMixin, forms.ModelForm):
     """Used for creating an entirely new entity."""
     def clean_name(self):
         name = self.cleaned_data['name']
-        if self._meta.model.objects.filter(name=name, author=self.user).exists():
+        if self._meta.model.objects.filter(name=name).exists():
             raise ValidationError(
                 'You already have a %s named "%s"' % (self._meta.model.display_type, name))
 
