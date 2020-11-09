7c3a4d4da78c53aaac81798e4bb0b7133ce9331d
Jonathan Cooper <j.p.cooper@ucl.ac.uk>
2020-11-09 13:30:54 +0000

    Python 3.5 support

--- weblab/config/settings/base.py
+++ weblab/config/settings/base.py
@@ -80,7 +80,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'templates',
+            str(BASE_DIR / 'templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
