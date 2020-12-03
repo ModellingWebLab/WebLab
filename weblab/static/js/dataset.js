33a3a520b4bbf1243cf758554777745235e46459
Steve Roderick <steve@roderick.com>
2020-12-03 10:51:44 +0000

    Corrected code indentation

--- weblab/static/js/dataset.js
+++ weblab/static/js/dataset.js
@@ -1,60 +1,58 @@
 function init() {
 
-  $("#toggle-older-versions").click(function() {
-    $(".older-version-mappings").toggle();
-    if ($(".older-version-mappings").is(":visible")) {
-      $(this).text("Hide older versions");
-    } else {
-      $(this).text("Show older versions");
-    }
-  });
+    $("#toggle-older-versions").click(function () {
+        $(".older-version-mappings").toggle();
+        if ($(".older-version-mappings").is(":visible")) {
+            $(this).text("Hide older versions");
+        } else {
+            $(this).text("Show older versions");
+        }
+    });
 
     var $visibility = $("#versionVisibility");
-  $visibility.on(
-      'change',
-      '#id_visibility',
-      function() {
-        updateVisibility (
-            $visibility.data('change-href'),
-            {
-              visibility: $(this).val(),
-            })
-      });
+    $visibility.on(
+        'change',
+        '#id_visibility',
+        function () {
+            updateVisibility(
+                $visibility.data('change-href'),
+                {
+                    visibility: $(this).val(),
+                })
+        });
 
 
 }
 
-function updateVisibility (url, jsonObject)
-  {
+function updateVisibility(url, jsonObject) {
     var $actionIndicator = $("#versionVisibilityAction");
-    $actionIndicator.html("<img src='"+staticPath+"img/loading2-new.gif' alt='loading' />");
+    $actionIndicator.html("<img src='" + staticPath + "img/loading2-new.gif' alt='loading' />");
 
     $.post(
         url,
         jsonObject,
-        function(json) {
+        function (json) {
             if (json.updateVisibility) {
-              var msg = json.updateVisibility.responseText;
-            if (json.updateVisibility.response) {
-              $actionIndicator.html(
-                  "<img src='"+staticPath+"img/check.png' alt='valid' /> " + msg);
-            } else {
-              $actionIndicator.html("<img src='"+staticPath+"img/failed.png' alt='invalid' /> " + msg);
+                var msg = json.updateVisibility.responseText;
+                if (json.updateVisibility.response) {
+                    $actionIndicator.html(
+                        "<img src='" + staticPath + "img/check.png' alt='valid' /> " + msg);
+                } else {
+                    $actionIndicator.html("<img src='" + staticPath + "img/failed.png' alt='invalid' /> " + msg);
+                }
             }
-          }
         }
-    ).fail(function() {
-        $actionIndicator.html("<img src='"+staticPath+"img/failed.png' alt='error' /> sorry, serverside error occurred.");
-    }).done(function(json){
+    ).fail(function () {
+        $actionIndicator.html("<img src='" + staticPath + "img/failed.png' alt='error' /> sorry, serverside error occurred.");
+    }).done(function (json) {
         notifications.display(json);
     })
-      .fail(function(){
-        notifications.add("sorry, server-side error occurred", "error");
-      });
-  }
-
+        .fail(function () {
+            notifications.add("sorry, server-side error occurred", "error");
+        });
+}
 
 
 module.exports = {
-  init: init,
+    init: init,
 }
