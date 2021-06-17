0cb537ae43da4795e960135662dd63604349a2c0
MauriceHendrix <52317399+MauriceHendrix@users.noreply.github.com>
2021-06-17 15:48:27 +0100

    Update processing.py

--- weblab/core/processing.py
+++ weblab/core/processing.py
@@ -3,6 +3,8 @@ from urllib.parse import urljoin
 from django.conf import settings
 
 def prepend_callback_base(url_path):
+    """Prepend CALLBACK_BASE_UR to url_path. If we are running in a subfolder this is striped from url_path."""
+
     if hasattr(settings, 'FORCE_SCRIPT_NAME'):
          url_path = url_path.replace(settings.FORCE_SCRIPT_NAME, '')
     return urljoin(settings.CALLBACK_BASE_URL, url_path)
