4fdf0c96c92d96f19e0b848b6fe4425e7b9598ad
MauriceHendrix <52317399+MauriceHendrix@users.noreply.github.com>
2021-06-17 18:05:03 +0100

    Update weblab/core/processing.py
    Co-authored-by: Jonathan Cooper <j.p.cooper@ucl.ac.uk>

--- weblab/core/processing.py
+++ weblab/core/processing.py
@@ -3,9 +3,12 @@ from urllib.parse import urljoin
 from django.conf import settings
 
 def prepend_callback_base(url_path):
-    """Prepend CALLBACK_BASE_UR to url_path. If we are running in a subfolder this is striped from url_path."""
+    """Prepend CALLBACK_BASE_URL to url_path.
+    
+    If we are running at a sub-URL (settings.FORCE_SCRIPT_NAME is set) then the prefix is stripped,
+    since the callback is done on a private interface (to localhost) not via the public WebLab address.
+    """
 
     if hasattr(settings, 'FORCE_SCRIPT_NAME') and settings.FORCE_SCRIPT_NAME is not None:
          url_path = url_path.replace(settings.FORCE_SCRIPT_NAME, '', 1)
     return urljoin(settings.CALLBACK_BASE_URL, url_path)
-
