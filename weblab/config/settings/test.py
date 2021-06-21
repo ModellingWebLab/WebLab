4d3dee2f23c8d0c75e07960b71468266a6592ecf
MauriceHendrix <52317399+MauriceHendrix@users.noreply.github.com>
2021-06-21 17:27:00 +0100

    Update test.py

--- weblab/config/settings/test.py
+++ weblab/config/settings/test.py
@@ -1,10 +1,4 @@
 from .base import *  # noqa
-try:
-    # when running in deployed mode we need to import database setting to prevent access errors
-    from .deployed import DATABASES  # noqa
-except ImportError:
-    # For CI tests we have no deployed settings
-    pass
 
 
 # Settings file used by pytest, whether locally or on Travis
