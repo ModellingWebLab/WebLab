fc3919fc7ebc219d69e391447315b973c710daeb
MauriceHendrix <52317399+MauriceHendrix@users.noreply.github.com>
2021-06-21 16:26:14 +0100

    Update test_processing.py

--- weblab/core/tests/test_processing.py
+++ weblab/core/tests/test_processing.py
@@ -1,13 +1,10 @@
 import pytest
 
-from django.conf import settings
 from core.processing import prepend_callback_base
 
 
 @pytest.mark.django_db
-def test_prepend_callback_base():
-    old_callback_base = settings.CALLBACK_BASE_URL
-    old_script_name = settings.FORCE_SCRIPT_NAME
+def test_prepend_callback_base(settings):
     settings.CALLBACK_BASE_URL = 'http://test.com/'
     settings.FORCE_SCRIPT_NAME = None
 
@@ -17,7 +14,3 @@ def test_prepend_callback_base():
     assert prepend_callback_base('/bla') == 'http://test.com/'
     assert prepend_callback_base('/bla/bla/morebla') == 'http://test.com/bla/morebla'
     assert prepend_callback_base('/experiments/bla/morebla') == 'http://test.com/experiments/bla/morebla'
-
-    settings.CALLBACK_BASE_URL = old_callback_base
-    settings.FORCE_SCRIPT_NAME = old_script_name
-
