e9504468561c92a9f5de72755e52cffd2a8d182e
MauriceHendrix <52317399+MauriceHendrix@users.noreply.github.com>
2021-06-21 14:34:43 +0100

    Update combine.py

--- weblab/core/combine.py
+++ weblab/core/combine.py
@@ -76,8 +76,7 @@ class ManifestWriter:
         else:
             format_ = self.identify_combine_format(path) or self.identify_mime_type(path)
 
-        #self._files.append((path, format_, is_master))
-        self._files.append((format_, path, is_master))
+        self._files.append((path, format_, is_master))
 
     @property
     def xml_doc(self):
