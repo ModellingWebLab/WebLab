a05eb8abf219490ad66624704c9d03bb3419db96
MauriceHendrix <52317399+MauriceHendrix@users.noreply.github.com>
2021-06-22 10:32:37 +0100

    Update vagrant.py

--- weblab/config/settings/vagrant.py
+++ weblab/config/settings/vagrant.py
@@ -4,3 +4,4 @@ from .deployed import DATABASES  # noqa
 
 # For running tests in a vagrant deployed environment run the test with `DJANGO_SETTINGS_MODULE=vagrant` in pytest.ini
 # This will use this file insetad of test.py
+# Make sure not to commit the modified pytest.ini!
