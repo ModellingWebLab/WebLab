2c246ecdac2c1fdf83ce955c156ae5bcc7e2347d
MauriceHendrix <52317399+MauriceHendrix@users.noreply.github.com>
2021-10-08 13:39:48 +0100

    Update weblab/accounts/views.py
    Co-authored-by: Jonathan Cooper <j.p.cooper@ucl.ac.uk>

--- weblab/accounts/views.py
+++ weblab/accounts/views.py
@@ -12,7 +12,7 @@ from django.conf import settings
 class RegistrationView(FormView):
     form_class = RegistrationForm
     template_name = 'registration/register.html'
-    success_url = settings.FORCE_SCRIPT_NAME if hasattr(settings, 'FORCE_SCRIPT_NAME') else '/'
+    success_url = getattr(settings, 'FORCE_SCRIPT_NAME', '/')
 
     class Meta:
         fields = ('email', 'full_name', 'institution')
