5ef83a222c5673b7e6ab927d3bc120bb3062e618
= <=>
2021-10-28 14:56:34 +0000

    added test cases for deleting experiemnts in use by a story

--- weblab/stories/tests/tests.py
+++ weblab/stories/tests/tests.py
@@ -1,28 +1,28 @@
-#import pytest
-#from django.test import TestCase
+# import pytest
+# from django.test import TestCase
 #
-#from core import recipes
+# from core import recipes
 #
 #
-#@pytest.fixture
-#def story(user):
+# @pytest.fixture
+# def story(user):
 #    return recipes.story.make(author=user)
 #
 #
-#@pytest.fixture
-#def story_text(user, story):
+# @pytest.fixture
+# def story_text(user, story):
 #    return recipes.story_text.make(author=user, story=story)
 #
 #
-#@pytest.fixture
-#def story_graph(user, story):
+# @pytest.fixture
+# def story_graph(user, story):
 #    pass
 #    return recipes.story_graph.make(author=user, story=story, graphfilename='outputs_Transmembrane_voltage_gnuplot_data.csv', 
 #                                     cachedprotocolversion=cachedprotocolversion, cachedmodelversions=[cachedmodelversion])
 #
 #
-#@pytest.mark.django_db
-#class TestExperimentsDeletion:
+# @pytest.mark.django_db
+# class TestExperimentsDeletion:
 #    def test_delete_experiment_in_use(self, story, story_text):
 #        pass
 #
