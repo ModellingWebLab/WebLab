562be0660715745cb26b924804d4fe72b2bbc8d1
Dr Maurice Hendrix <52317399+MauriceHendrix@users.noreply.github.com>
2021-11-01 18:26:45 +0000

    Update test_forms.py

--- weblab/stories/tests/test_forms.py
+++ weblab/stories/tests/test_forms.py
@@ -38,7 +38,10 @@ class TestStoryCollaboratorForm:
         assert not form.is_valid()
 
         experiment.protocol_version.visibility = 'public'
-        experiment.model_version.visibility = 'public'
+        experiment.protocol_version.save()
+        form = self._form({'email': other_user.email, 'DELETE': False}, story)
+        assert not form.is_valid()
+        
         experiment.protocol_version.save()
         experiment.model_version.save()
         assert experiment.protocol_version.protocol.is_version_visible_to_user(experiment.protocol_version.sha,
