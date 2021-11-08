d6b743532e60e5aa1750f902741c39bf12e45c4f
Dr Maurice Hendrix <52317399+MauriceHendrix@users.noreply.github.com>
2021-11-08 11:12:23 +0000

    Delete test_apps.py

--- weblab/stories/tests/test_apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import apps
-from django.test import TestCase
-from stories.apps import StoriesConfig
-
-
-class ReportsConfigTest(TestCase):
-    def test_apps(self):
-        self.assertEqual(StoriesConfig.name, 'stories')
-        self.assertEqual(apps.get_app_config('stories').name, 'stories')
