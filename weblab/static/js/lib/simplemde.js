8ca0ad3bb626ed3cd9e7f0346a3b509bcfe5ef17
Dr Maurice Hendrix <52317399+MauriceHendrix@users.noreply.github.com>
2021-11-15 11:06:00 +0000

    Update simplemde.js

--- weblab/static/js/lib/simplemde.js
+++ weblab/static/js/lib/simplemde.js
@@ -1,26 +1,4 @@
-/*The MIT License (MIT)
-
-Copyright (c) 2015 Next Step Webs, Inc.
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.*/
-
-//Modified version of SimpleMDE, see https://simplemde.com/
+//This is a modified version of SimpleMDE, see https://simplemde.com/. SimpleMde is distributed under MIT license (see SimpleMDE-license), these changes are included with the project under the main project's license, see LIECENSE.
 //changed for weblab: require statements below, drawImage function, var insertTexts and require paths inside dependancies
 
 /*global require,module*/
