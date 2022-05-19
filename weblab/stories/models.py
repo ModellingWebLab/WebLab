8978498e19e3a24ffaa9a24a025d9031b52bd89d
= <=>
2022-05-19 15:57:51 +0000

    added delete back to stories

--- weblab/stories/models.py
+++ weblab/stories/models.py
@@ -69,38 +69,40 @@ class StoryGraph(StoryItem):
     cachedprotocolversion = models.ForeignKey(CachedProtocolVersion, null=False, blank=False, on_delete=models.CASCADE,
                                               related_name="protocolforgraph")
     cachedmodelversions = models.ManyToManyField(CachedModelVersion)
-    modelgroup = models.ForeignKey(ModelGroup, blank=True, null=True, default=None, on_delete=models.SET_DEFAULT)
+    modelgroups = models.ManyToManyField(ModelGroup, blank=True)
+    grouptoggles = models.ManyToManyField(ModelGroup, blank=True)
+#    modelgroup = models.ForeignKey(ModelGroup, blank=True, null=True, default=None, on_delete=models.SET_DEFAULT)
 
-    def __str__(self):
-        return (self.modelgroup.title if self.modelgroup is not None
-                else self.cachedmodelversions.first().model.name) +\
-            f' / {self.cachedprotocolversion.protocol.name} / {self.graphfilename}'
+#    def __str__(self):
+#        return (self.modelgroup.title if self.modelgroup is not None
+#                else self.cachedmodelversions.first().model.name) +\
+#            f' / {self.cachedprotocolversion.protocol.name} / {self.graphfilename}'
 
-    def set_cachedmodelversions(self, cachedmodelversions):
-        self.setting_cachedmodelversions = True
-        self.save()
-        self.cachedmodelversions.set(cachedmodelversions)
-        self.setting_cachedmodelversions = False
-        self.save()
+#    def set_cachedmodelversions(self, cachedmodelversions):
+#        self.setting_cachedmodelversions = True
+#        self.save()
+#        self.cachedmodelversions.set(cachedmodelversions)
+#        self.setting_cachedmodelversions = False
+#        self.save()
 
-
-@receiver(m2m_changed, sender=StoryGraph.cachedmodelversions.through)
-def storygraph_constraints(sender, **kwargs):
-    """
-    enforce constriants for StoryGraph
-    """
-    action = kwargs.pop('action', '')
-    instance = kwargs.pop('instance', None)
-    if action.startswith('post') and not getattr(instance, 'setting_cachedmodelversions', False):
-        # must have at least 1 cachedmodelversion
-        if instance.cachedmodelversions.count() == 0:
-            raise IntegrityError("StoryGraph must have model")
-        # if using mdel group must have exactly 1 cachedmodelversion
-        if instance.modelgroup is None and instance.cachedmodelversions.count() != 1:
-            raise IntegrityError("StoryGraph without modelgroup must have 1 model")
-        # models in modelgroup must be the same as the ones referenced in cachedmodelversions
-        if instance.modelgroup is not None:
-            models_in_group = sorted(instance.modelgroup.models.all(), key=str)
-            models_in_graph = sorted([m.model for m in instance.cachedmodelversions.all()], key=str)
-            if models_in_group != models_in_graph:
-                raise IntegrityError("Models in modelgroup and the cachedmodelversions don't match")
+#
+#@receiver(m2m_changed, sender=StoryGraph.cachedmodelversions.through)
+#def storygraph_constraints(sender, **kwargs):
+#    """
+#    enforce constriants for StoryGraph
+#    """
+#    action = kwargs.pop('action', '')
+#    instance = kwargs.pop('instance', None)
+#    if action.startswith('post') and not getattr(instance, 'setting_cachedmodelversions', False):
+#        # must have at least 1 cachedmodelversion
+#        if instance.cachedmodelversions.count() == 0:
+#            raise IntegrityError("StoryGraph must have model")
+#        # if using mdel group must have exactly 1 cachedmodelversion
+#        if instance.modelgroup is None and instance.cachedmodelversions.count() != 1:
+#            raise IntegrityError("StoryGraph without modelgroup must have 1 model")
+#        # models in modelgroup must be the same as the ones referenced in cachedmodelversions
+#        if instance.modelgroup is not None:
+#            models_in_group = sorted(instance.modelgroup.models.all(), key=str)
+#            models_in_graph = sorted([m.model for m in instance.cachedmodelversions.all()], key=str)
+#            if models_in_group != models_in_graph:
+#                raise IntegrityError("Models in modelgroup and the cachedmodelversions don't match")
