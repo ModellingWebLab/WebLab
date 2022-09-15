1dd09b9d516f2d560ea4b7c5f9480d6acfac2707
Dr Maurice Hendrix <52317399+MauriceHendrix@users.noreply.github.com>
2022-09-15 17:28:17 +0100

    Update vagrant.py

--- weblab/config/settings/vagrant.py
+++ weblab/config/settings/vagrant.py
@@ -5,6 +5,6 @@ from .deployed import DATABASES  # noqa
 # For running tests in a vagrant deployed environment
 # run the test with `DJANGO_SETTINGS_MODULE=config.settings.vagrant` in pytest.ini
 # This will use this file insetad of test.py
-# You may also need to allow database creation on postgres, for changing permissions see: 
+# You may also need to allow database creation on postgres, for changing permissions see:
 # https://stackoverflow.com/questions/10845998/i-forgot-the-password-i-entered-during-postgres-installation
 # Make sure not to commit the modified pytest.ini!
