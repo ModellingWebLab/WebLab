a4a2d0e5f2a7a1b49c995d2a5bc2a73ef5ed40fc
MauriceHendrix <maurice.hendrix@gmail.com>
2022-09-15 09:15:01 +0000

    better instructions for testing in vagrant

--- weblab/config/settings/vagrant.py
+++ weblab/config/settings/vagrant.py
@@ -2,6 +2,6 @@ from .base import *  # noqa
 from .deployed import DATABASES  # noqa
 
 
-# For running tests in a vagrant deployed environment run the test with `DJANGO_SETTINGS_MODULE=vagrant` in pytest.ini
+# For running tests in a vagrant deployed environment run the test with `DJANGO_SETTINGS_MODULE=config.settings.vagrant` in pytest.ini
 # This will use this file insetad of test.py
 # Make sure not to commit the modified pytest.ini!
