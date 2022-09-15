4e7966e106060f63a4e928b64989ab52f83f4233
Maurice Hendrix <maurice.hendrix@nottingham.ac.uk>
2022-09-15 17:20:58 +0100

    flake8

--- weblab/config/settings/test.py
+++ weblab/config/settings/test.py
@@ -2,6 +2,7 @@ from .base import *  # noqa
 
 
 # Settings file used by pytest, whether locally or on github actions
-# For running tests in a vagrant deployed environment run the test with `DJANGO_SETTINGS_MODULE=config.settings.vagrant` in pytest.ini
+# For running tests in a vagrant deployed environment
+# run the test with `DJANGO_SETTINGS_MODULE=config.settings.vagrant` in pytest.ini
 # (see vagrant.py)
 # Make sure not to commit the modified pytest.ini!
