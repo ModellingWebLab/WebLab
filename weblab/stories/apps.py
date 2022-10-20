a27a973a9f4b6fbaac92968dd3b86d3ce9bcde23
MauriceHendrix <maurice.hendrix@gmail.com>
2022-10-20 15:06:07 +0000

    improved code coverage for stories

--- weblab/stories/apps.py
+++ weblab/stories/apps.py
@@ -1,27 +1,5 @@
 from django.apps import AppConfig
-from django.db.models.signals import post_delete
 
 
 class StoriesConfig(AppConfig):
     name = 'stories'
-
-    def ready(self):
-        """
-        Executed when stories app has bee loaded.
-        """
-        from entities.models import ModelEntity, ModelGroup
-        from .models import StoryGraph
-
-        def model_deleted(sender, instance, **kwargs):
-            """
-            Signal callback when an entity is about to be deleted.
-            This signal specificaly deals with cases where model groups ans tory graphs
-            are left hanging after user deletion.
-            """
-            # delete story graphs without cached model versions
-            StoryGraph.objects.filter(cachedmodelversions=None).delete()
-            # delete model groups without models
-            ModelGroup.objects.filter(models=None).delete()
-
-        # Connect signal to post_save
-        post_delete.connect(model_deleted, ModelEntity)
